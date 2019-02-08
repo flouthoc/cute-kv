59265d088d1bed26caaec1c9ccc6e31707bf74f2
kasisnu <kasisnu@gmail.com>
2019-02-08 11:20:14 +0530

    Gofmt-d the code

--- server/server.go
+++ server/server.go
@@ -1,89 +1,86 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"net/http"
+	"bytes"
 	"encoding/gob"
-    "io/ioutil"
-    "bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
-
 //core node responsible for storing value;
-type CuteNode struct{
-	Key string
+type CuteNode struct {
+	Key   string
 	Value []byte
 }
 
 var cute_kv_global_map map[string]CuteNode
 
-func cute_kv_greet(w http.ResponseWriter, r *http.Request){
+func cute_kv_greet(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, cute_kv_global_map["a"].Value)
 	w.Write([]byte("<html><head></head><body>Cute-kv: Is a minimal quick <h4>in-memory</h4> kv store <br>Usage:<br>/get?key=<key> : Will Return Value as response otherwise blank.<br>/set?key=<key>&value=<value> : Will set k:v. Overrides existing key, will return true on sucess.</body></html>"))
 }
 
-func cute_kv_set_query(w http.ResponseWriter, r *http.Request){
+func cute_kv_set_query(w http.ResponseWriter, r *http.Request) {
 
 	formkey := r.FormValue("key")
 	formvalue := r.FormValue("value")
-	cutenode := CuteNode{Key :formkey, Value: []byte(formvalue)}
+	cutenode := CuteNode{Key: formkey, Value: []byte(formvalue)}
 	cute_kv_global_map[formkey] = cutenode
 	w.Write([]byte("true"))
 }
 
-
 // This function will not be used when running database as in-memory datastore
-func cute_kv_set_query_flushable(w http.ResponseWriter, r *http.Request){
+func cute_kv_set_query_flushable(w http.ResponseWriter, r *http.Request) {
 
 	formkey := r.FormValue("key")
 	formvalue := r.FormValue("value")
-	cutenode := CuteNode{Key :formkey, Value: []byte(formvalue)}
+	cutenode := CuteNode{Key: formkey, Value: []byte(formvalue)}
 	cute_kv_global_map[formkey] = cutenode
 	internal_cute_kv_flush_to_disk()
 	w.Write([]byte("true"))
 }
 
-func cute_kv_get(w http.ResponseWriter, r *http.Request){
+func cute_kv_get(w http.ResponseWriter, r *http.Request) {
 
 	cutenode, ok := cute_kv_global_map[r.FormValue("key")]
 
-	if ok{
+	if ok {
 
 		w.Write(cutenode.Value)
 
-	}else{
+	} else {
 
 		w.Write([]byte("nil"))
 	}
 
 }
 
-func cute_kv_health(w http.ResponseWriter, r *http.Request){
+func cute_kv_health(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("healthy"))
 }
 
-
 // This function will not be used when running database as in-memory datastore
 
-func internal_cute_kv_load_from_disk(){
+func internal_cute_kv_load_from_disk() {
 
 	dat, err := ioutil.ReadFile("dat")
-	if err != nil{
+	if err != nil {
 		fmt.Println("Unable to Read static data")
 		fmt.Println("Try Running application with sudo access")
-        fmt.Println(err)
-	}else{
+		fmt.Println(err)
+	} else {
 
 		buf := bytes.NewBuffer(dat)
 		d := gob.NewDecoder(buf)
 		err2 := d.Decode(&cute_kv_global_map)
-		if err2 != nil{
+		if err2 != nil {
 
 			fmt.Println("Unable to Decode static data")
 			fmt.Println("Try Running application with sudo access")
-        	fmt.Println(err2)
+			fmt.Println(err2)
 		}
 	}
 
@@ -91,35 +88,34 @@ func internal_cute_kv_load_from_disk(){
 
 // This function will not be used when running database as in-memory datastore
 
-func internal_cute_kv_flush_to_disk(){
+func internal_cute_kv_flush_to_disk() {
 
 	b := new(bytes.Buffer)
-    e := gob.NewEncoder(b)
-    err := e.Encode(cute_kv_global_map)
-    if err != nil {
-    	fmt.Println("Try Running application with sudo access")
-        fmt.Println(err)
-        fmt.Println("Something went wrong while serializing the map")
-    }else{
-    	err2 := ioutil.WriteFile("dat", b.Bytes(), 0777)
-    	if err2 != nil{
-    		fmt.Println("Try Running application with sudo access")
-        	fmt.Println(err2)
-        	fmt.Println("Something went wrong while writing map to file")
+	e := gob.NewEncoder(b)
+	err := e.Encode(cute_kv_global_map)
+	if err != nil {
+		fmt.Println("Try Running application with sudo access")
+		fmt.Println(err)
+		fmt.Println("Something went wrong while serializing the map")
+	} else {
+		err2 := ioutil.WriteFile("dat", b.Bytes(), 0777)
+		if err2 != nil {
+			fmt.Println("Try Running application with sudo access")
+			fmt.Println(err2)
+			fmt.Println("Something went wrong while writing map to file")
 
-    	}
-    }
+		}
+	}
 
 }
 
-
-func main(){
+func main() {
 
 	port := "9992"
 	persistance := "false"
-	if(len(os.Args) > 1){
+	if len(os.Args) > 1 {
 		port = os.Args[1]
-		if(len(os.Args) > 2){
+		if len(os.Args) > 2 {
 			persistance = os.Args[2]
 		}
 	}
@@ -130,11 +126,10 @@ func main(){
 	if persistance == "true" {
 
 		//fill map with contents from disk
-		internal_cute_kv_load_from_disk();
+		internal_cute_kv_load_from_disk()
 		http.HandleFunc("/set", cute_kv_set_query_flushable)
 
-		
-	}else{
+	} else {
 
 		http.HandleFunc("/set", cute_kv_set_query)
 	}
@@ -143,7 +138,7 @@ func main(){
 	http.HandleFunc("/get", cute_kv_get)
 	http.HandleFunc("/health", cute_kv_health)
 
-	fmt.Println("Staring Cute-kv server on port "+port+", visit \n http://127.0.0.1:"+port+"/ \n or http://0.0.0.0:"+port+"/ \n or http://localhost:"+port+"/")
+	fmt.Println("Staring Cute-kv server on port " + port + ", visit \n http://127.0.0.1:" + port + "/ \n or http://0.0.0.0:" + port + "/ \n or http://localhost:" + port + "/")
 
 	err := http.ListenAndServe(":"+port, nil)
 
